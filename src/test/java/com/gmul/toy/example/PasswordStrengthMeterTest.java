335cb352cb283161e7444bbb43a4dca55e32925e
sijin <sijin.choi@way2bit.com>
2020-10-15 00:03:33 +0900

    practice test code - password strength

--- src/test/java/com/gmul/toy/example/PasswordStrengthMeterTest.java
+++ src/test/java/com/gmul/toy/example/PasswordStrengthMeterTest.java
@@ -10,6 +10,13 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class PasswordStrengthMeterTest {
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+
     // 1. 빈 테스트 메서드로 테스트 환경을 갖추었는지 테스트
 //    @Test
 //    public void name() {
@@ -21,10 +28,45 @@ public class PasswordStrengthMeterTest {
     // 테스트 코드가 통과할 만큼만 코드 추rk
     @Test
     public void 모든_조건충족시_암호강도_강함 () {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, result);
-        PasswordStrength result2 = meter.meter("abc1!Add");
-        assertEquals(PasswordStrength.STRONG, result2);
+//        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//        PasswordStrength result = meter.meter("ab12!@AB");
+//        assertEquals(PasswordStrength.STRONG, result);
+//        PasswordStrength result2 = meter.meter("abc1!Add");
+//        assertEquals(PasswordStrength.STRONG, result2);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
+    }
+
+    // 3. 지금까지의 테스트를 통과하도록 수정
+    @Test
+    public void 길이만_8글자미만_나머지충족() {
+//        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//        PasswordStrength result = meter.meter("ab12!@A");
+//        assertEquals(PasswordStrength.NORMAL, result);
+//        PasswordStrength result2 = meter.meter("Ab12!c");
+//        assertEquals(PasswordStrength.NORMAL, result2);
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
+    }
+
+    // 4
+    @Test
+    public void 숫자_포함X_나머지충족() {
+//        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//        PasswordStrength result = meter.meter("ab!@ABqwer");
+//        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+    }
+
+    // 5 각 단계별 테스트 코드가 모두 통과하는지 확인해라
+    @Test
+    public void 값이_없음() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    // 6
+    @Test
+    public void 대문자_포함X_나머지충족() {
+        assertStrength("ab12!@df", PasswordStrength.NORMAL);
     }
 }
--- src/test/java/com/gmul/toy/example/domain/PasswordStrengthMeter.java
+++ src/test/java/com/gmul/toy/example/domain/PasswordStrengthMeter.java
@@ -5,7 +5,64 @@ import com.gmul.toy.example.type.PasswordStrength;
 public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String s) {
-//        return null;
+
+        // 5
+        if(s == null) {
+            return PasswordStrength.INVALID;
+        }
+
+        // 1
+        // return null
+
+        // 3
+        if(s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+
+        // 4 => method 구성
+//        boolean containsNum = false;
+//        for(char ch : s.toCharArray()) {
+//            if(ch >= '0' && ch <= '9') {
+//                containsNum = true;
+//                break;
+//            }
+//        }
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        if(!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
+
+        // 6
+//        boolean containsUpp = false;
+//        for(char ch : s.toCharArray()) {
+//            if(Character.isUpperCase(ch)) {
+//                containsUpp = true;
+//                break;
+//            }
+//        }
+        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(!containsUpp) {
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingNumberCriteria(String s) {
+        for(char ch : s.toCharArray()) {
+            if(ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String s) {
+        for(char ch : s.toCharArray()) {
+            if(Character.isUpperCase(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
